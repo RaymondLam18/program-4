327881072de7339773d5ae3f6fdf493d8d98133f
RaymondLam18 <1024322@hr.nl>
2023-05-23 14:04:54 +0200

    Update retro.js

--- src/js/retro.js
+++ src/js/retro.js
@@ -1,30 +1,77 @@
-import { Actor, Vector, Engine, Random } from "excalibur";
+import { Actor, Vector, Engine, Random, Input } from "excalibur";
 import { Resources } from "./resources.js";
 
 export class Retro extends Actor {
-    onInitialize(engine) {
-      this.anchor = new Vector(0, 0);
-      this.rand = new Random();
-      this.graphics.use(Resources.Retro.toSprite());
-      this.w = Resources.Retro.width;
-      this.h = Resources.Retro.height;
-      this.pos = new Vector(
-        this.rand.integer(this.w, engine.drawWidth - this.w),
-        this.rand.integer(this.h, engine.drawHeight - this.h)
-      );
-      this.vel = new Vector(Math.random() * 80 - 40, Math.random() * 80 - 40);
-  
-      this.angularVelocity = Math.random() + 0.2;
-      this.rotation = 12;
-    }
-  
-    onPostUpdate(engine) {
-      if (this.pos.x < 0 || this.pos.x + this.w > engine.drawWidth) {
-        this.vel.x = -this.vel.x;
-      }
-      if (this.pos.y < 0 || this.pos.y + this.h > engine.drawHeight) {
-        this.vel.y = -this.vel.y;
-      }
+  onInitialize(engine) {
+    this.anchor = new Vector(0, 0);
+    this.rand = new Random();
+    this.graphics.use(Resources.Retro.toSprite());
+    this.scale = new Vector(0.5, 0.5); // Adjust the scale to make the Retro actor smaller
+    this.w = Resources.Retro.width * this.scale.x;
+    this.h = Resources.Retro.height * this.scale.y;
+    
+    // Set the initial position to the left side of the screen
+    this.pos = new Vector(
+      this.w,
+      this.rand.integer(this.h, engine.drawHeight - this.h)
+    );
+
+    this.vel = new Vector(0, 0);
+
+    // Register keyboard event handlers for arrow keys and WASD keys
+    engine.input.keyboard.on('hold', this.onKeyDown.bind(this));
+    engine.input.keyboard.on('release', this.onKeyUp.bind(this));
+  }
+
+  onKeyDown(evt) {
+    // Start moving Retro when the arrow key or WASD key is pressed
+    switch (evt.key) {
+      case Input.Keys.Up:
+      case Input.Keys.W:
+        this.vel.y = -100;
+        break;
+      case Input.Keys.Left:
+      case Input.Keys.A:
+        this.vel.x = -100;
+        break;
+      case Input.Keys.Down:
+      case Input.Keys.S:
+        this.vel.y = 100;
+        break;
+      case Input.Keys.Right:
+      case Input.Keys.D:
+        this.vel.x = 100;
+        break;
     }
   }
-  
+
+  onKeyUp(evt) {
+    // Stop moving Retro when the arrow key or WASD key is released
+    switch (evt.key) {
+      case Input.Keys.Up:
+      case Input.Keys.Down:
+      case Input.Keys.W:
+      case Input.Keys.S:
+        this.vel.y = 0;
+        break;
+      case Input.Keys.Left:
+      case Input.Keys.Right:
+      case Input.Keys.A:
+      case Input.Keys.D:
+        this.vel.x = 0;
+        break;
+    }
+  }
+
+  onPostUpdate(engine) {
+    const newPos = this.pos.add(this.vel.scale(engine.deltaTime / 1000));
+
+    // Check if the new position is within the screen boundaries
+    if (newPos.x >= 0 && newPos.x + this.w <= engine.drawWidth) {
+      this.pos.x = newPos.x;
+    }
+    if (newPos.y >= 0 && newPos.y + this.h <= engine.drawHeight) {
+      this.pos.y = newPos.y;
+    }
+  }
+}
